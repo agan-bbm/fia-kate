f478b6c2179b32dda5e43f9dbc25d798ee5b08e3
agan-bbm <agan@blackbird.marketing>
2022-04-27 16:34:23 +0200

    about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Homepage from "./components/Pages/Homepage";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import { About } from "./components/Shared/Pages/About";
 console.log(
   "%c Developed by: Black Bird Marketing https://blackbird.marketing",
   "padding:9px 5px; background:black; line-height:25px;color: white"
@@ -9,7 +11,10 @@ console.log(
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
